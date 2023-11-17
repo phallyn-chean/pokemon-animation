5f567ecbfab94fd451f1f4062c05d97385bc5614
phallyn-chean <phollyn.chean@student.cadt.edu.kh>
2023-11-17 15:50:06 +0700

    add pokemon state

---
+++ lib/states/pokemon/pokemon_event.dart
@@ -0,0 +1,15 @@
+abstract class PokemonEvent {
+  const PokemonEvent();
+}
+
+class PokemonLoadStarted extends PokemonEvent {
+  final bool loadAll;
+  const PokemonLoadStarted({this.loadAll = false});
+}
+
+class PokemonLoadMoreStarted extends PokemonEvent {}
+
+class PokemonSelectChanged extends PokemonEvent {
+  final String pokemonId;
+  const PokemonSelectChanged({required this.pokemonId});
+}
